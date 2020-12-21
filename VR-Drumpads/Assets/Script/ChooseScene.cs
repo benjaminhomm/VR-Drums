2d9b4da4e607c8a11d13deac1c79aba958c20f01
benjaminhomm <73250819+benjaminhomm@users.noreply.github.com>
2020-12-21 10:43:00 +0100

    Initial Unity Project commit

---
+++ VR-Drumpads/Assets/Script/ChooseScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseScene : MonoBehaviour
+{
+
+    public void StartSpace()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartNature()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartConcert()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ VR-Drumpads/Assets/Script/GenerateSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GenerateSound : MonoBehaviour
+{
+    public AudioSource drumBeat;
+    public GameObject Particle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drumBeat = GetComponent<AudioSource> ();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        drumBeat.PlayOneShot(drumBeat.clip);
+        Particle.GetComponent<ParticleSystem>().Play();
+    }
+}
