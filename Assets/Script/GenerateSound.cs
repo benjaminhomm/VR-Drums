ae8f1e9a2dd0d9b35291149ce733e249b8d94ff1
benjaminhomm <73250819+benjaminhomm@users.noreply.github.com>
2021-01-05 12:33:37 +0100

    Wrong usage of gitignore

---
+++ Assets/Script/GenerateSound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class GenerateSound : MonoBehaviour
+{
+    public AudioSource drumBeat;
+    public GameObject Particle;
+
+    private Interactable interactable;
+    private SteamVR_Input_Sources hand;
+
+    void Start()
+    {
+        drumBeat = GetComponent<AudioSource> ();
+        interactable = GetComponent<Interactable>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        drumBeat.PlayOneShot(drumBeat.clip);
+        Particle.GetComponent<ParticleSystem>().Play();
+
+        if (interactable.attachedToHand)
+        {
+            hand = interactable.attachedToHand.handType;
+            interactable.attachedToHand.TriggerHapticPulse(3500);
+        }    
+    }
+}
